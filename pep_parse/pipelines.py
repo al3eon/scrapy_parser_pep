8808e0ced170a4e44f9de3fbb3095b6095dee12a
aleon <azaretov123@gmail.com>
2025-09-06 11:53:19 +0300

    all work

--- pep_parse/pipelines.py
+++ pep_parse/pipelines.py
@@ -1,41 +1,40 @@
 import csv
 import os
+from collections import defaultdict
 from datetime import datetime
 from pathlib import Path
-
-from itemadapter import ItemAdapter
-
-from tests.test_files import BASE_DIR
 
 
 class PepParsePipeline:
 
     def open_spider(self, spider):
-        self.count_status = {}
-        BASE_DIR = Path(__file__).parent.parent
-        RESULTS_DIR = BASE_DIR / 'results'
-        RESULTS_DIR.mkdir(exist_ok=True)
+        self.count_status = defaultdict(int)
+        self.settings = spider.settings
+        self.base_path = None
+        if self.settings.get('FEEDS'):
+            for feed_path in self.settings.get('FEEDS', {}).keys():
+                feed_path_str = str(feed_path)
+                if '%(time)s' in feed_path_str:
+                    self.base_path = os.path.dirname(feed_path_str)
+                    break
+        if not self.base_path:
+            self.base_path = 'results'
+        Path(self.base_path).mkdir(parents=True, exist_ok=True)
 
     def process_item(self, item, spider):
-        if item['status'] in self.count_status:
-            self.count_status[item['status']] += 1
-        else:
-            self.count_status[item['status']] = 1
-
+        self.count_status[item['status']] += 1
         return item
 
     def close_spider(self, spider):
-        BASE_DIR = Path(__file__).parent.parent
-        RESULTS_DIR = BASE_DIR / 'results'
-        RESULTS_DIR.mkdir(exist_ok=True)
-        file_name = datetime.now().strftime('status_summary_%Y-%m-%d_%H:%M:%S.csv')
-        file_path = RESULTS_DIR / file_name
+        file_name = datetime.now().strftime(
+            'status_summary_%Y-%m-%d_%H:%M:%S.csv')
+        file_path = Path(self.base_path) / file_name
 
         with open(file_path, mode='w', encoding='utf-8') as f:
             writer = csv.writer(f, lineterminator='\n')
             writer.writerow(['Статус', 'Количество'])
             for status, count in self.count_status.items():
                 writer.writerow([status, count])
+
             total = sum(self.count_status.values())
             writer.writerow(['Total', total])
-
